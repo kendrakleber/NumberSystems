8ada7416a56599015017a6f8bc3b5c05e5ef371b
S <league@iMac.attlocal.net>
2016-05-04 18:27:09 -0700

    adding things

--- src/Decimal.java
+++ src/Decimal.java
@@ -1,10 +1,17 @@
 
 import java.io.PrintWriter;
 
+import javax.swing.JOptionPane;
+
 public class Decimal {
+	private String input;
+	private int number;
+	private StringBuilder output = new StringBuilder();
+	private static String MESSAGE = "Input Your Number";
+	private PrintWriter pw;
 
 	public Decimal(PrintWriter pw) {
-		// TODO Auto-generated constructor stub
+		this.pw = pw;
 	}
 
 	public void decToBin() {
@@ -12,38 +19,42 @@ public class Decimal {
 		toBin();
 		outBin();
 	}
-	
+
 	public void decToHex() {
 		inputDec();
 		toHex();
 		outHex();
 	}
-	
+
 	private void outHex() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void toHex() {
-		// TODO Auto-generated method stub
-		
+		char[] numbers = input.toCharArray();
+		for (int i=0;i<numbers.length;i++) {
+			if (Integer.parseInt(""+numbers[i]) > 6) {
+				output.append(numbers[i]);
+			} else if(Integer.parseInt(""+numbers[i]) != 1 && Integer.parseInt(""+numbers[i+1]) != 1){
+				output.append(numbers[i]);
+			}
+		}
 	}
 
 	private void outBin() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void toBin() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void inputDec() {
-		// TODO Auto-generated method stub
-		
+		input = JOptionPane.showInputDialog(null, MESSAGE);
+		number = Integer.parseInt(input);
 	}
-
-	
 
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,5 @@
+
+
 import java.io.PrintWriter;
 
 import javax.swing.JOptionPane;
@@ -7,10 +9,18 @@ public class Menu {
 	String returnChoice;
 	
 	public Menu(PrintWriter pw) {
+<<<<<<< HEAD
+=======
+		// TODO Auto-generated constructor stub
+>>>>>>> b7932eb4e3b7f880dfa03e89eb6d88fa0a2fc8fe
 		this.pw = pw;
 	}
 
 	public void display() {
+<<<<<<< HEAD
+=======
+		// TODO Auto-generated method stub
+>>>>>>> b7932eb4e3b7f880dfa03e89eb6d88fa0a2fc8fe
 		String choice = JOptionPane.showInputDialog("Type 1 for decimal to binary, type 2 for decimal to hexadecimal,"
 				+ "type 3 for binary to decimal, type 4 for binary to hexadecimal, type 5 for hexadeimal to decimal"
 				+ "type 6 for hexadecimal to binary");
@@ -19,6 +29,11 @@ public class Menu {
 	}
 
 	public int getSelection() {
+<<<<<<< HEAD
+=======
+		// TODO Auto-generated method stub
+		return 0;
+>>>>>>> b7932eb4e3b7f880dfa03e89eb6d88fa0a2fc8fe
 		int i = Integer.parseInt(returnChoice);
 		if(i >= 1 && i <=6) 
 		return i;
@@ -26,6 +41,7 @@ public class Menu {
 		return 7;
 	}
 
+<<<<<<< HEAD
 }
 /*
 choice = menu.getSelection();
@@ -37,3 +53,6 @@ case 4: bin.binToHex(); break;
 case 5: hex.hexToDec(); break;
 case 6: hex.hexToBin(); break;
 }*/
+=======
+}
+>>>>>>> b7932eb4e3b7f880dfa03e89eb6d88fa0a2fc8fe
