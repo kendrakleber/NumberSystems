9482e81f6920807ed41f62c9acd5a525fd0963c6
William Bonitz <wbnhw3@gmail.com>
2016-06-08 18:05:51 -0700

    Binary class works
    nibbling bites

--- src/Binary.java
+++ src/Binary.java
@@ -8,8 +8,9 @@ public class Binary {
 
 	private int num;
 	private PrintWriter pw = null;
-	String s;
+	String s; //s is the number as a string
 	int dec;
+	String hex;
 	
 
 	public Binary(PrintWriter pw) {
@@ -30,18 +31,42 @@ public class Binary {
 	}
 
 	private void outHex() {
-		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, s + " in hexadecimal is: " + hex);
 
 	}
-
+	
 	private void toHex() {
-		// TODO Auto-generated method stub
-
+		String sum = "";
+		for(int i = 0; i < 32; i+=4) {
+			String nibble = s.substring(i, i+4);
+			int j = 0;
+			int sum1 = 0;
+			while(j < 4) {
+				int thisDigit = Integer.parseInt(nibble.charAt(4-j-1) + "");
+				sum1 += thisDigit * Math.pow(2.0, j);
+				j++;
+			}
+			if(sum1 == 10) {
+				sum+="A";
+			} else if(sum1 == 11) {
+				sum+="B";
+			} else if(sum1 == 12) {
+				sum+="C";
+			} else if(sum1 == 13) {
+				sum+="D";
+			} else if(sum1 == 14) {
+				sum+="E";
+			} else if(sum1 == 15) {
+				sum+="F";
+			} else {
+				sum+=sum1;
+			}
+		}
+		hex = sum;
 	}
 
 	private void outDec() {
 		JOptionPane.showMessageDialog(null, s + " in decimal is: " + dec);
-
 	}
 
 	private void toDec() {
--- src/test.java
+++ src/test.java
@@ -1,14 +1,34 @@
 
 public class test {
 	public static void main(String[] args) {
-		String s = "00000000000000000000000000000100";
-		int sum = 0;
-		int i = 0;
-		while(i < 32) {
-			int thisDigit = Integer.parseInt(s.charAt(32-i-1) + "");
-			sum += thisDigit * Math.pow(2.0, i);
-			i++;
+		String s = "0000000000000000000000000001111";
+		String sum = "";
+		for(int i = 0; i < 32; i+=4) {
+			String nibble = s.substring(i, i+4);
+			int j = 0;
+			int sum1 = 0;
+			while(j < 4) {
+				int thisDigit = Integer.parseInt(nibble.charAt(4-j-1) + "");
+				sum1 += thisDigit * Math.pow(2.0, j);
+				j++;
+			}
+			if(sum1 == 10) {
+				sum+="A";
+			} else if(sum1 == 11) {
+				sum+="B";
+			} else if(sum1 == 12) {
+				sum+="C";
+			} else if(sum1 == 13) {
+				sum+="D";
+			} else if(sum1 == 14) {
+				sum+="E";
+			} else if(sum1 == 15) {
+				sum+="F";
+			} else {
+				sum+=sum1;
+			}
 		}
+		
 		System.out.println(sum);
 		
 	}
