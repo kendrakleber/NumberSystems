e7f8a86ab3af324f1ca85bccccfcf376ad612c6d
William Bonitz <wbnhw3@gmail.com>
2016-06-08 18:19:41 -0700

    switched the order of cases
    switched case 5 and 6 so they are in a logical order

--- src/Menu.java
+++ src/Menu.java
@@ -21,9 +21,10 @@ public class Menu {
 			System.out.println(x);
 		}
 		String choice = JOptionPane.showInputDialog("Type 1 for decimal to binary, type 2 for decimal to hexadecimal,"
-				+ "type 3 for binary to decimal, type 4 for binary to hexadecimal, type 5 for hexadeimal to decimal"
-				+ "type 6 for hexadecimal to binary");
+				+ "type 3 for binary to decimal, type 4 for binary to hexadecimal, type 5 for hexadeimal to binary"
+				+ "type 6 for hexadecimal to decimal");
 		returnChoice = choice;
+	
 		
 	}
 
